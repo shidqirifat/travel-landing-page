06e8b0c2f74d043105243fe9418ff38f556d4c20
shidqirifat <rifatpangestu87@gmail.com>
2021-11-16 10:50:41 +0700

    tambah section experience, video, dan place. buat html, css, dan js event (video play pause)

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ const headerNav = document.querySelector("header nav");
 const btnTop = document.querySelector(".btn-top");
 
 const links = document.querySelectorAll(".link");
-const sections = document.querySelectorAll("section");
+const sections = document.querySelectorAll("section[id]");
 
 const activeLink = () => {
   let sectionLength = sections.length;
@@ -16,6 +16,7 @@ const activeLink = () => {
 
   links[sectionLength].classList.add("open");
   menuNav.classList.remove("active");
+  closeNav.classList.remove("active");
 }
 
 window.addEventListener("scroll", activeLink);
@@ -41,8 +42,42 @@ window.onscroll = function () {
 
 menuBtn.addEventListener("click", () => {
   menuNav.classList.add("active");
+  closeNav.classList.add("active");
 });
 
 closeNav.addEventListener("click", () => {
   menuNav.classList.remove("active");
+  closeNav.classList.remove("active");
 });
+
+
+// VIDEO
+const videoFile = document.querySelector(".video-file"),
+  videoButton = document.querySelector(".video-button"),
+  videoIcon = document.querySelector(".video-icon");
+
+const playPause = () => {
+  if (videoFile.paused) {
+    videoFile.play();
+
+    videoIcon.classList.remove("fa-play");
+    videoIcon.classList.add("fa-pause");
+  } else {
+    videoFile.pause();
+
+    videoIcon.classList.add("fa-play");
+    videoIcon.classList.remove("fa-pause");
+  }
+  console.log('object');
+}
+
+const endVideo = () => {
+  videoFile.currentTime = 0;
+
+  videoIcon.classList.add("fa-play");
+  videoIcon.classList.remove("fa-pause");
+}
+
+videoButton.addEventListener("click", playPause);
+
+videoFile.addEventListener("ended", endVideo);
